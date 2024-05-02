379babf48701d158de7a1bb5e0477b2befb33333
Tristan Misko <TMisko@thinkbrg.com>
2024-05-02 04:00:11 -0700

    Initialized challenge repository for sharing log

---
+++ scripts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+alias start="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "start:" >> ~/ChallengeLog/challenge.log"
+alias stop="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "stop:" >> ~/ChallengeLog/challenge.log"
