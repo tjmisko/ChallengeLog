cef315a4b8661962949237bcc07376a975a4fa11
Tristan Misko <TMisko@thinkbrg.com>
2024-05-02 11:27:17 -0700

    Tristan's 2024-05-02 11:27:17 status update

--- scripts.sh
+++ scripts.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 alias start="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "start:" >> ~/ChallengeLog/challenge.log"
+
 alias stop="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "stop:" >> ~/ChallengeLog/challenge.log"
+
 work_intervals() {
     cat challenge.log | \
     awk '{ print $2" "$3 }' | \
     xargs -I {} date -d "{}" '+%s' | \
     awk '{ if (NR % 2 == 1) { odd = $0; } else { result = $0 - odd; print int(result/36)/100 " hours"; } }'
 }
+
 running_total() {
     work_intervals | awk '{ sum+=$1 } END { print sum" hours" }'
 }
+
 status_update() {
     running_total | awk '{ print "# Tristan is currently at "$1" hours!"}' > readme.md
     echo -e "\r" >> readme.md
-    cat challenge.log >> readme.md
+    cat challenge.log | sed -e 's/\r/\r\r/' >> readme.md
     cat challenge.log
     git commit -am "Tristan's $(date +%Y-%m-%d\ %H\:%M\:%S) status update"
     git push origin tristan
