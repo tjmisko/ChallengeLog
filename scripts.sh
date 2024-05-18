aeda34d581eb2dca32b08951223e3e5a82ff831c
Tristan Misko <TMisko@thinkbrg.com>
2024-05-18 04:22:12 -0700

    Tristan's 2024-05-18 04:22:11 status update

--- scripts.sh
+++ scripts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 alias start="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "start:" >> ~/ChallengeLog/challenge.log"
-
 alias stop="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "stop:" >> ~/ChallengeLog/challenge.log"
 
 work_intervals() {
-    cat challenge.log | \
+    local CHALLENGE_DIR = "~/ChallengeLog"
+    cat $CHALLENGE_DIR/challenge.log | \
     awk '{ print $2" "$3 }' | \
     xargs -I {} date -d "{}" '+%s' | \
     awk '{ if (NR % 2 == 1) { odd = $0; } else { result = $0 - odd; print int(result/36)/100 " hours"; } }'
@@ -15,10 +15,11 @@ running_total() {
 }
 
 status_update() {
-    running_total | awk '{ print "# Tristan is currently at "$1" hours!"}' > readme.md
-    cat challenge.log | sed -e 's/stop/Tristan stopped work at/' | sed -e 's/start/Tristan started work at/' | sed 's/T/* T/' >> readme.md
+    local CHALLENGE_DIR = "~/ChallengeLog"
+    running_total | awk '{ print "# Tristan is currently at "$1" hours!"}' > $CHALLENGE_DIR/readme.md
+    cat $CHALLENGE_DIR/challenge.log | sed -e 's/stop/Tristan stopped work at/' | sed -e 's/start/Tristan started work at/' | sed 's/T/* T/' >> readme.md
     echo -e "\n"
-    cat challenge.log
+    cat $CHALLENGE_DIR/challenge.log
     echo -e "\n"
     echo "Currently at: $(running_total)"
     echo -e "\n"
