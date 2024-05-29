4e978cee4ff96256fc7e10e49ae30e2815c155bc
Tristan Misko <tristanmisko@gmail.com>
2024-05-30 00:13:42 +0100

    Tristan's 2024-05-29 16:13:41 status update

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 * Tristan stopped work at: 2024-05-27 12:30:06
 * Tristan started work at: 2024-05-28 10:28:16
 * Tristan stopped work at: 2024-05-28 10:31:54
+* Tristan started work at: 2024-05-29 16:13:31
+* Tristan stopped work at: 2024-05-29 16:13:33
--- scripts.sh
+++ scripts.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-alias start="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "start:" >> C:/Users/TMisko/ChallengeLog/challenge.log"
-alias stop="date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "stop:" >> C:/Users/TMisko/ChallengeLog/challenge.log"
-export CHALLENGE_DIR="C:/Users/TMisko/ChallengeLog"
+export CHALLENGE_DIR="C:/Users/TMisko/Tools/ChallengeLog"
 
-work_intervals() {
+usage() {
+    echo unknown command: "$@"
+    echo Usage: clog start|stop|intervals|total|update
+}
+
+start() {
+    date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "start:" >> C:/Users/TMisko/Tools/ChallengeLog/challenge.log
+}
+
+stop() {
+    date +%Y-%m-%d\ %H\:%M\:%S | xargs echo "stop:" >> C:/Users/TMisko/Tools/ChallengeLog/challenge.log
+}
+
+intervals() {
     cat $CHALLENGE_DIR/challenge.log | \
     awk '{ print $2" "$3 }' | \
     xargs -I {} date -d "{}" '+%s' | \
     awk '{ if (NR % 2 == 1) { odd = $0; } else { result = $0 - odd; print int(result/36)/100 " hours"; } }'
 }
 
-running_total() {
+total() {
     work_intervals | awk '{ sum+=$1 } END { print sum" hours" }'
 }
 
-status_update() {
+update() {
     running_total | awk '{ print "# Tristan is at "$1" hours this week!"}' > $CHALLENGE_DIR/readme.md
     cat $CHALLENGE_DIR/challenge.log | sed -e 's/stop/Tristan stopped work at/' | sed -e 's/start/Tristan started work at/' | sed 's/T/* T/' >> readme.md
     echo -e "\n"
@@ -25,3 +36,14 @@ status_update() {
     git commit -am "Tristan's $(date +%Y-%m-%d\ %H\:%M\:%S) status update"
     git push origin tristan
 }
+
+declare -A COMMANDS=(A
+    [main]=usage
+    [start]=start
+    [stop]=stop
+    [intervals]=intervals
+    [total]=total
+    [update]=update
+)
+
+"${COMMANDS[${1:-main}]:-${COMMANDS[main]}}" "$@"
